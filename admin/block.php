a02b46deb4f8fc61c914863f70e8efd29fe92e20
Nasrin Jahan <43464259+nasrin264@users.noreply.github.com>
2018-12-04 05:57:48 -0800

    Add files via upload

---
+++ admin/block.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+       <link href="library.css" rel="stylesheet" type="text/css"/>
+        <title> head </title>
+    </head>
+    <body style="background-color: #9ACD32">
+        <div id="header"> 
+            <h2 style="padding: 10px"> <font face="Forte">Happy Reading</font></h2>
+        </div>
+        <div id="nav"> 
+            <ul>
+                <li><b><a href="index.php">Home</a></b></li>
+                <li><b><a href="trp.php">Top Rated Books</a></b></li>
+                <li><b><a href="block.php">Block</a></b></li>
+                <li><b><a href="contect.php">Contect Us</a></b></li>
+                <li><b><a href="about.php">About Us</a></b></li>
+                <li><b><a href="help.php">Help</a></b></li>
+            </ul>  
+        </div>   
+        <div id="container"><br><br>
+            <div id="bol">
+                <p <font color="red"><i><b>My project HD</b></i></b> </font></p>
+                <br>
+                <p style="text-align: right;margin-right: 5px">My first block</p>
+                <p><a href="#" style="color: red">Like 1</a> <a href="#" style="color: red">Unlike 0</a></p>
+            </div>  
+            
+        </div>
+        <div id="footer"> <br><br><br><br><br><br><br><br><br><br><br><br>
+            <h2 style="margin-top: 300px;" align="center">My projet HD</h2>
+        </div>
+        
+        <?php
+        // put your code here
+        ?>
+       
+    </body>
+</html>
---
+++ admin/index2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+       <link href="library.css" rel="stylesheet" type="text/css"/>
+        <title> head </title>
+    </head>
+    <body style="background-color: #9ACD32">
+        <div id="header"> 
+            <h2 style="padding: 10px"> <font face="Forte">Happy Reading</font></h2>
+        </div>
+        <div id="nav"> 
+            <ul>
+                <li><b><a href="index.php">Home</a></b></li>
+                <li><b><a href="trp.php">Top Rated Books</a></b></li>
+                <li><b><a href="block.php">Block</a></b></li>
+                <li><b><a href="contect.php">Contact Us</a></b></li>
+                <li><b><a href="about.php">About Us</a></b></li>
+                <li><b><a href="help.php">Help</a></b></li>
+            </ul>  
+        </div>   
+        <div id="container">
+            <h2 style="padding: 10px">Admin Login:</h2>
+            <br><br><center>
+                <table>
+                    <tr>
+                        <td width="200px" height="50px">User Name </td>
+                        <td width="200px" height="50px"><input type="text" name="un" placeholder="Enter Username" </td>
+                    </tr>
+                    <tr>
+                        <td width="200px" height="50px">Password </td>
+                        <td width="200px" height="50px"><input type="password" name="pass" placeholder="Enter Password" </td>
+                    </tr>
+                </table>
+            </center>
+        </div>
+        <div id="footer"> <br><br><br><br><br><br><br><br><br><br><br><br>
+            <h2 style="margin-top: 300px;" align="center">My projet HD</h2>
+        </div>
+        
+        <?php
+        // put your code here
+        ?>
+       
+    </body>
+</html>
